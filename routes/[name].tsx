5f0f6a0ce4739ed1463e817b6b43e61f4aff33a5
Youngho Cho <dominoyh5@gmail.com>
2022-07-10 14:25:27 +0000

    ~2.9

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -2,6 +2,12 @@
 import { h } from "preact";
 import { PageProps } from "$fresh/server.ts";
 
-export default function Greet(props: PageProps) {
-  return <div>Hello {props.params.name}</div>;
+export default function GreetPage(props: PageProps) {
+  const { name } = props.params;
+  
+  return (
+    <main>
+      <p>Greetings to you, {name}!</p>
+    </main>
+  )
 }
